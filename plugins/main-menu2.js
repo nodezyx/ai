aa60fb9a6d984d861eb2fdc6f9fd088a44699d64
nodezyx <subzeronodes@gmail.com>
2025-08-19 21:13:11 +0200

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -169,8 +169,8 @@ ${menuSections}
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
-                    // forwardingScore: 999,
-                   // isForwarded: true,
+                     forwardingScore: 999,
+                    isForwarded: true,
                     forwardedNewsletterMessageInfo: {
                         newsletterJid: '120363304325601080@newsletter',
                         newsletterName: '𝐒𝐔𝐁𝐙𝐄𝐑𝐎 𝐌𝐃 𝐕𝟓',
